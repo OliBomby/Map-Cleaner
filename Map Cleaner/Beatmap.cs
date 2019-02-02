95a9e8512a7af610d2aef72bfa7962a6c2c70db1
OliBomby <olivierschipper@gmail.com>
2019-02-02 13:12:10 +0100

    Refactor

--- Map Cleaner/Beatmap.cs
+++ Map Cleaner/Beatmap.cs
@@ -7,46 +7,6 @@ using System.Threading.Tasks;
 
 namespace Map_Cleaner
 {
-    class TValue
-    {
-        public string StringValue { get; set; }
-        public dynamic Value { get => GetValue(); set => SetValue(value); }
-
-        public TValue(string str)
-        {
-            StringValue = str;
-        }
-
-        public dynamic GetValue()
-        {
-            if (double.TryParse(StringValue, out double d))
-            {
-                if(StringValue.Split('.').Count() > 1)
-                {
-                    return d;
-                }
-                else
-                {
-                    return int.Parse(StringValue);
-                }
-            }
-            else
-            {
-                return StringValue;
-            }
-        }
-
-        public void SetValue(dynamic value)
-        {
-            StringValue = value.ToString();
-        }
-
-        public string GetStringValue()
-        {
-            return StringValue;
-        }
-    }
-
     class Beatmap
     {
         public Dictionary<string, TValue> General { get; set; }
